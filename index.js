4c029dd0dcd0bd1abc1d0ad7c592b64b8e915bbf
Jin mo Kim <skim112790@yahoo.com>
2021-10-21 20:34:39 -0400

    Added code for annual budget

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    weeklyExpenses = weeklyExpenses + numberAnswer
+}
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    monthlyExpenses = monthlyExpenses + numberAnswer
+}
+for (let i = 0; i < annualExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    annualExpenses = annualExpenses + numberAnswer
+}
